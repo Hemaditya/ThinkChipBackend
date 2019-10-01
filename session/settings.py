863d3f09a2b13219dea913cf15001098a545481a
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-01 18:10:04 +0530

    Session for settings

---
+++ session/settings.py
@@ -0,0 +1,10 @@
+"""
+    This is module which holds global settings for session that are created
+"""
+
+"""
+    This value is the number of epochs for current session and will be updated when creating a new session.
+    EPOCHS variable will only be used while recording the data and/or doing real time analysis and not while 
+    postprocessing.
+"""
+EPOCHS = 0
