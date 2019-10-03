89312d5eaab056c835dbe381e6d87718a4454e7b
Sashi Kanth Bezawada <sashi.bezawada@gmail.com>
2019-10-03 12:30:13 +0530

    Added Hanning window function to iterator

--- utils/iterator.py
+++ utils/iterator.py
@@ -1,25 +1,28 @@
 '''
     This module holds different iterators you can use to perform the sliding window on the data
 '''
+import numpy as np
 
 class Iterator():
     """
         Creates an iterator for your sliding window
     """
-    def __init__(self,data,chunk_size=250,hop=125):
+    def __init__(self, data, chunk_size=250, hop=125, window=True):
         """
             Inputs:
                 data:{type:np.ndarray, shape=(epochs*chunk_size)}
                     - Input Data
                 chunk_size:{type:int}
-                    - the amount of samples from the start of the hop 
+                    - the amount of samples from the start of the hop
                 hop:{type:int}
                     - the amount jump in the indices
         """
 
         self.data = data
-        self.chunk_size = chunk_size    
+        self.chunk_size = chunk_size
         self.hop = hop
+        if window:
+            self.window = np.hanning(chunk_size)
 
     def __iter__(self):
         """
@@ -30,10 +33,16 @@ class Iterator():
 
     def __next__(self):
         # Check if you have reached the end of the data
-        if(self.idx+self.chunk_size <= self.data.shape[0]):
-            nextData = self.data[self.idx:(self.idx+self.chunk_size)]  
+        if self.idx+self.chunk_size <= self.data.shape[0]:
+
+            nextData = self.data[self.idx:(self.idx+self.chunk_size)]
+
+            try:
+                nextData = nextData*self.window
+            except AttributeError:
+                pass
+
             self.idx = self.idx + self.hop
             return nextData
-        else:
-            raise StopIteration
 
+        raise StopIteration
