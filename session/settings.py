8a455d5d76e1f7a8ce4dfb92b48e6fb7b123360e
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-04 14:00:44 +0530

    [SESSION] Modified settings to show proper messages when something happens

--- session/settings.py
+++ session/settings.py
@@ -15,10 +15,17 @@ EPOCHS = 0
 'The path to this Application settings is APP_PATH'
 import os
 from pathlib import Path
-APP_PATH = Path(os.environ['HOME'])/'.config'
+APP_PATH = Path(os.environ['HOME'])/'.config'/'ThinkChip'
+if(not os.path.exists(APP_PATH)):
+    print(f"CONSOLE: the App path does not exist")
+    print(f"CONSOLE: Creating App path at {APP_PATH}")
+    os.mkdir(APP_PATH)
+
 'Session data will be stored in the path SESSION_PATH'
 SESSION_PATH = APP_PATH/'SessionData'
 if(os.path.exists(SESSION_PATH) == False):
     ' If the path to session data does not exist, the create it'
+    print(f"CONSOLE: The Session path does not exist")
+    print(f"CONSOLE: Creating Session path at {SESSION_PATH}")
     os.mkdir(SESSION_PATH)
 
