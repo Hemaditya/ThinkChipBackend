5f736d3ec1e134d945c26a9eb2eaf19292929657
Sashi Kanth Bezawada <sashi.bezawada@gmail.com>
2019-10-04 17:41:00 +0530

    Rough implementation of real time attention widget

--- features/feature_removal.py
+++ features/feature_removal.py
@@ -16,7 +16,7 @@ def energy_of_epoch(data):
         About this function:
             This function finds the energy per epoch.
     """
-    
+
     energy = np.square(data)
     energy = np.mean(energy)
     return energy
@@ -26,7 +26,7 @@ def remove_bad_epochs(data,threshold=None, channels=[0],sliding_window=True):
         Inputs:
             data:{type:np.ndarray, shape:(epochs,config.CHANNELS,config.CHUNK_SIZE)}
             threshold:{type:int}
-                - The threshold to test for bad epoch against 
+                - The threshold to test for bad epoch against
             channels:{type:list}
                 - The channels to test this on
             sliding_window:{type:boolean}
@@ -50,20 +50,24 @@ def remove_bad_epochs(data,threshold=None, channels=[0],sliding_window=True):
         else:
             dataIterator.append(data[c])
 
-    bad_epochs = [] 
+    bad_epochs = []
     for c in channels:
         for D in dataIterator[c]:
             for i,d in enumerate(D):
                 if(energy_of_epoch(d) >= threshold):
                     bad_epochs.append(i)
-               
-    
+
+
     # Remove duplicate epochs
     bad_epochs = np.array(list(set(bad_epochs)))
-    if(sliding_window == True):
-        # Generate indices for bad_epochs in oldData
-        bad_epochs = bad_epochs*dataIterator[0].hop 
-        bad_epochs = np.floor(bad_epochs/dataIterator[0].chunk_size)
-    data = np.delete(oldData,bad_epochs,axis=0)
-        
+    # if(sliding_window == True):
+    #     # Generate indices for bad_epochs in oldData
+    #     bad_epochs = bad_epochs*dataIterator[0].hop
+    #     bad_epochs = np.floor(bad_epochs/dataIterator[0].chunk_size)
+    # data = np.delete(oldData,bad_epochs,axis=0)
+
+    ## Make a method in Iterator class which gives array (epochs, channels, chunk_size)
+    ## Then create new array = np.array(epochs-bad_epochs, channels, chunk_size)
+    ## Return this new array
+
     return data
