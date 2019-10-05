cb38f81695ff98f99d6aba3ea53827f10c66ea1e
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-05 14:49:57 +0530

    Fixed path faults

--- widgets/ENG.py
+++ widgets/ENG.py
@@ -1,17 +1,19 @@
 '''
     The widget to measure user engagement
 '''
-
 import time
+import os
 import numpy as np
 import threading
 import filters as f
+import pathlib
 import pickle
 import config
 from widgets import Widget
 # Make sure the global config file knows about this widget
 import features as ft
 
+# Game Class
 class Game():
     """
         This will in general hold the media stats and settings of each of the media
@@ -33,17 +35,21 @@ class Game():
         with open(self.game_path/"filtered_data.pickle", 'wb') as f:
             pickle.dump(data,f)
 
+# ENG class
 class ENG(Widget):
     """
 
     """
 
+    # Init function
     def __init__(self,widget_name, widget_path, sessionName=None, offline=False):
         """
             Initialize a new widget object and create its path and other stuff
         """
+        self.widget_name = widget_name
+        self.widget_path = widget_path
         super().__init__(self.widget_name,self.widget_path)
-        self.session_id, self.session_path = self.create_session(sessionName)
+        self.session_id, self.session_path = self.create_session()
         self.games = list(np.load("games.npy"))
 
         # Initialize the data reader for realtime data processing
@@ -71,7 +77,6 @@ class ENG(Widget):
             return 0
         games.remove(game_name)
 
-
     def check_game_exists(self, game):
         if(game not in os.listdir(self.session_path)):
             return 0
@@ -84,9 +89,11 @@ class ENG(Widget):
         """
 
         # First check if the game exists or not
-        if not check_game_exists(game_name):
+        if not self.check_game_exists(game_name):
             print("CONSOLE: creating a folder for {game_name} in session {self.session_id}")
-        game_path = self.session_id/game_name
+
+
+        game_path = self.session_id/pathlib.Path(game_name)
         return game_path
 
     def create_session(self,sessionName=None):
@@ -95,23 +102,21 @@ class ENG(Widget):
             All the data for this session will be stored in this folder
         """
         session_id = sessionName
-        if(sessioName == None):
+        if(sessionName == None):
             session_id = time.strftime("%d%m%y_%H%M%S")
-
         print("CONSOLE: Create a new session {}".format(session_id))
         session_path = self.widget_path/session_id
         os.mkdir(session_path)
         return session_id, session_path
 
-    def start_session(self,game_name, trails=120):
+    def start_session(self,game_name="test", trails=120):
         """
             In this function, this widget is supposed to start reading data and keep saving it
             in its session folder every 10 seconds.
-
             This function is the command line version
         """
         # Check if the game exists if not, then create a new game
-        game_path = self.session_path/game_name
+        game_path = self.session_path/pathlib.Path(game_name)
         if not self.check_game_exists(game_name):
             game_path = self.create_game_session(game_name)
 
