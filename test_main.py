80478eb89038dc5d51f7a0d2c447fda4928608ce
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-08 18:28:21 +0530

    [test] test for completely readingthe data

--- test_main.py
+++ test_main.py
@@ -1,7 +1,11 @@
 import session
+from widgets import ENG
 
 session.create_user('raghav')
 session.delete_user('raghav')
 session.create_user('raghav')
-#session.add_widget('raghav','eng')
-session.delete_widget('raghav','eng')
+session.add_widget('raghav','eng')
+
+widget_path = session.check_widget('raghav','eng')
+eng = ENG(widget_path,20)
+eng.run()
