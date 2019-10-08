d4cf052aa181799ad33e2bf9a9eb6bebf8515b72
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-08 16:50:29 +0530

    [TEST] wrote test script to create users

--- test_main.py
+++ test_main.py
@@ -1,5 +1,3 @@
-import widgets
-import pathlib
-path = pathlib.Path('./test_data/ENG')
-ENG = widgets.ENG.ENG('ENG',path)
-ENG.test_start_session('test',channels=[0,1],read_length=5)
+import session
+
+session.create_user('raghav')
