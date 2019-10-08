2528fab6b553c4134d2fd29f849e4cd4545ca578
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-08 17:00:13 +0530

    [test] for adding and deleting users and widgets

--- test_main.py
+++ test_main.py
@@ -1,3 +1,7 @@
 import session
 
 session.create_user('raghav')
+session.delete_user('raghav')
+session.create_user('raghav')
+#session.add_widget('raghav','eng')
+session.delete_widget('raghav','eng')
