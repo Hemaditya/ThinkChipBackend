351e9a799dfa503487be8a10e1d284255b192fba
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-10 13:30:27 +0530

    added a function to load widget data

--- data/load_data.py
+++ data/load_data.py
@@ -1,11 +1,14 @@
 """
     This module will load offline data to process it.
+    This module works only for ENG widgets
 """
 import os
 import sys
 import pickle
 import pathlib
 import config
+import session
+import numpy as np
 
 def load_data(username):
     ''' Load data of the user username '''
@@ -37,4 +40,51 @@ def load_data(username):
 
     return data
 
+def load_widget_data(user, widget):
+    ''' This function adds a new widget to the user '''
+
+    """
+        user: str or Path object
+        - if user is a string, it is assumed to be username.
+        - if user is a Path object, it is assumed to be the path to the user's directory
+
+        Outputs:
+        - Widget data dictionary
+        - for each of the game, a dictionary will be created with timestamp as its key and
+            the values will be its data.
+    """
+
+    widget_path = session.check_widget(user,widget)
+
+    if widget_path == 0:
+        print(f"CONSOLE: Cannot load the data for {widget} as it does not exist")
+        return 0
+
+    if os.listdir(widget_path) == 0:
+        print(f"CONSOLE: There is no data present in {widget}") 
+        return 0
+
+    games = np.load('../widgets/games.npy')
+
+    widget_data = {}
+    for g in games:
+        widget_data[g] = {}
+
+    #  Start loding the contents in the wiget_path
+    game_files = os.listdir(widget_path)
+
+    for g in game_files:
+        # First Extract the game name
+        game_name = g.split('|')[0][3:]
+        # Get the timestamp of that sample
+        timestamp = g.split('|')[1].split('.')[0] 
+        
+        # Get the numpy data
+        game_f = open(widget_path/g,'rb')
+        game_data = pickle.load(game_f)
+
+        # Put data in in the dictionary
+        widget_data[game_name][timestamp] = game_data
+
+    return widget_data
 
