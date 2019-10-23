2e485755b7778d35a559b3d60ea4b2b545dee1f5
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-10-23 15:09:52 +0530

    Basic functions added to view data

--- data/load_data.py
+++ data/load_data.py
@@ -1,90 +1,54 @@
 """
     This module will load offline data to process it.
-    This module works only for ENG widgets
 """
 import os
 import sys
 import pickle
-import pathlib
+from pathlib import Path
 import config
-import session
 import numpy as np
+import matplotlib.pyplot as plt
 
-def load_data(username):
-    ''' Load data of the user username '''
-    '''
-        Inputs:
-            username: string
-                - The username of the person who's data you want to load.
+def load_all_pickle_files(path,vstack=False):
+    # This function will load the data from all the pickle files in the given path variable
+    # If vstack is true, then it will combine data of all files into a single numpy array
+    # by stacking them one upon other
+    path = Path(path)
+    if (os.path.exists(path)):
+        print(f"Loading data from {path} ")
+        pass
+    else:
+        print(f"The path: {path} does not exist")
+        return 
 
-        Outputs:
-            data: dictionary
-                - Keys are the widget names and values are the contents of the widget
-
-    '''
-    path = config.SESSION_DIR
-    username = username.lower()
-    path = path/username
-    if not os.path.isdir(username):
-        print(f"CONSOLE: The username: {username} does not exist")
-        return 0
-
-    widgtes = os.listdir(path) 
-    data = {}
-    for w in widgets:
-        data[w] = {}
-        w_path = path/w
-        for u in os.listdir(w_path):
-            with open(w_path/u,'rb') as f:
-                data[w][u] = pickle.load(f)
-
+    data = []
+    for file in os.listdir(path):
+        if 'pickle' in file:
+            print(f"Loading File: {file}")
+            with open(path/file,'rb') as f_:
+                d = pickle.load(f_)
+                data.append(d)
+       
+    if(vstack == True):
+        print(f"Using vstack=True")
+        data = np.vstack(data)
+    else:
+        data = np.array(data)
     return data
 
-def load_widget_data(user, widget):
-    ''' This function adds a new widget to the user '''
+def plot_signal(data,channels=[0]):
+    """ This function will plot the signal """
 
-    """
-        user: str or Path object
-        - if user is a string, it is assumed to be username.
-        - if user is a Path object, it is assumed to be the path to the user's directory
-
-        Outputs:
-        - Widget data dictionary
-        - for each of the game, a dictionary will be created with timestamp as its key and
-            the values will be its data.
-    """
-
-    widget_path = session.check_widget(user,widget)
-
-    if widget_path == 0:
-        print(f"CONSOLE: Cannot load the data for {widget} as it does not exist")
-        return 0
-
-    if os.listdir(widget_path) == 0:
-        print(f"CONSOLE: There is no data present in {widget}") 
-        return 0
-
-    games = np.load('../widgets/games.npy')
-
-    widget_data = {}
-    for g in games:
-        widget_data[g] = {}
-
-    #  Start loding the contents in the wiget_path
-    game_files = os.listdir(widget_path)
-
-    for g in game_files:
-        # First Extract the game name
-        game_name = g.split('|')[0][3:]
-        # Get the timestamp of that sample
-        timestamp = g.split('|')[1].split('.')[0] 
+    w,h = len(channels)*7, len(channels)*8
+    fig = plt.figure(figsize=(w,h))
+    
+    nrows = len(channels)//2 + 1
+    ncols = 2
+    
+    for i,c in enumerate(channels):
+        ax = fig.add_subplot(nrows,ncols,i+1)
+        ax.plot(data[:,c,:].reshape(-1))
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Amplitube")
+        ax.set_title(f"Channel: {c}")
         
-        # Get the numpy data
-        game_f = open(widget_path/g,'rb')
-        game_data = pickle.load(game_f)
-
-        # Put data in in the dictionary
-        widget_data[game_name][timestamp] = game_data
-
-    return widget_data
-
