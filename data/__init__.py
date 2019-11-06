6658ca4c1f651b99be56b9b45c615da89354c72d
raghavendra galeppa <Raghavendrar404@gmail.com>
2019-11-06 16:54:57 +0530

    I did a commiting

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from .load_data import *
 from .attention_metrics import *
 from .plotting_functions import *
+from .max_val import *
--- features/__init__.py
+++ features/__init__.py
@@ -3,4 +3,3 @@ sys.path.append('../')
 
 from features.bandpower import *
 from features.feature_removal import *
-from .max_val import *
